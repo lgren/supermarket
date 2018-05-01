b69a934c651772bbcb1952878defb92c2ea34324
lgren <625552409@qq.com>
2018-05-01 14:59:06 +0800

    默认的

--- supermarket.sql
+++ supermarket.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50721
  File Encoding         : 65001
 
- Date: 27/04/2018 14:25:57
+ Date: 01/05/2018 14:58:02
 */
 
 SET NAMES utf8mb4;
@@ -73,7 +73,7 @@ CREATE TABLE `tb_cart` (
   `cart_goods` bigint(20) DEFAULT NULL,
   `user_id` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`cart_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of tb_cart
@@ -90,7 +90,6 @@ INSERT INTO `tb_cart` VALUES (8, 4, 8);
 INSERT INTO `tb_cart` VALUES (9, 4, 9);
 INSERT INTO `tb_cart` VALUES (10, 4, 10);
 INSERT INTO `tb_cart` VALUES (11, 4, 11);
-INSERT INTO `tb_cart` VALUES (16, NULL, 14);
 COMMIT;
 
 -- ----------------------------
@@ -104,18 +103,17 @@ CREATE TABLE `tb_cart_goods` (
   `cart_goods_time` datetime DEFAULT NULL,
   `number` int(11) DEFAULT '1',
   `price` double(11,3) DEFAULT '0.000',
-  PRIMARY KEY (`cart_goods_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+  `type` int(1) DEFAULT '0',
+  `want_pay_time` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`cart_goods_id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=111 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of tb_cart_goods
 -- ----------------------------
 BEGIN;
-INSERT INTO `tb_cart_goods` VALUES (2, 1, 2, NULL, 1, 3.000);
-INSERT INTO `tb_cart_goods` VALUES (3, 2, 3, NULL, 1, 4.000);
-INSERT INTO `tb_cart_goods` VALUES (4, 3, 2, NULL, 1, 3.200);
-INSERT INTO `tb_cart_goods` VALUES (5, 4, 4, NULL, 1, 6.500);
-INSERT INTO `tb_cart_goods` VALUES (11, 1, 1, NULL, NULL, NULL);
+INSERT INTO `tb_cart_goods` VALUES (109, 1, 7, '2018-05-01 14:10:39', 1, NULL, 1, 1525155313795);
+INSERT INTO `tb_cart_goods` VALUES (110, 1, 5, '2018-05-01 14:10:43', 1, NULL, 0, NULL);
 COMMIT;
 
 -- ----------------------------
@@ -127,7 +125,7 @@ CREATE TABLE `tb_collect` (
   `user_id` bigint(20) DEFAULT NULL,
   `collect_goods_id` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`collect_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of tb_collect
@@ -145,6 +143,7 @@ INSERT INTO `tb_collect` VALUES (9, 9, 4);
 INSERT INTO `tb_collect` VALUES (10, 10, 3);
 INSERT INTO `tb_collect` VALUES (11, 11, 2);
 INSERT INTO `tb_collect` VALUES (16, 14, NULL);
+INSERT INTO `tb_collect` VALUES (17, 15, NULL);
 COMMIT;
 
 -- ----------------------------
@@ -157,18 +156,13 @@ CREATE TABLE `tb_collect_goods` (
   `goods_id` bigint(20) DEFAULT NULL,
   `collect_goods_time` datetime DEFAULT NULL,
   PRIMARY KEY (`collect_goods_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of tb_collect_goods
 -- ----------------------------
 BEGIN;
-INSERT INTO `tb_collect_goods` VALUES (2, 1, 3, NULL);
-INSERT INTO `tb_collect_goods` VALUES (3, 2, 2, NULL);
-INSERT INTO `tb_collect_goods` VALUES (4, 3, 5, NULL);
-INSERT INTO `tb_collect_goods` VALUES (5, 4, 2, NULL);
-INSERT INTO `tb_collect_goods` VALUES (6, 5, 3, NULL);
-INSERT INTO `tb_collect_goods` VALUES (10, 1, 1, NULL);
+INSERT INTO `tb_collect_goods` VALUES (16, 1, 2, '2018-05-01 02:37:05');
 COMMIT;
 
 -- ----------------------------
@@ -191,12 +185,11 @@ CREATE TABLE `tb_goods` (
 -- Records of tb_goods
 -- ----------------------------
 BEGIN;
-INSERT INTO `tb_goods` VALUES (1, 1, '', '可乐', 1, 3.400, 0.80, NULL);
 INSERT INTO `tb_goods` VALUES (2, 1, NULL, '娃哈哈', 1, 4.000, 1.00, NULL);
 INSERT INTO `tb_goods` VALUES (3, 1, NULL, '柠檬乐事薯片', 1, 8.900, 1.00, NULL);
 INSERT INTO `tb_goods` VALUES (4, 1, NULL, '徐福记沙琪玛', 1, 23.000, 1.00, NULL);
 INSERT INTO `tb_goods` VALUES (5, 1, NULL, '海苔味乐事薯片', 1, 14.700, 1.00, NULL);
-INSERT INTO `tb_goods` VALUES (6, 2, NULL, '阿迪达斯球衣', 2, 599.000, 1.00, NULL);
+INSERT INTO `tb_goods` VALUES (6, 2, '', '阿迪达斯球衣', 2, 599.000, 0.85, NULL);
 INSERT INTO `tb_goods` VALUES (7, 2, NULL, '李宁鞋', 2, 1999.000, 0.90, NULL);
 COMMIT;
 
@@ -214,12 +207,30 @@ CREATE TABLE `tb_order` (
   `pay_way` int(3) DEFAULT '1',
   `order_time` datetime DEFAULT NULL,
   `send_goods` int(1) DEFAULT '0',
-  `send_goods_time` datetime DEFAULT NULL,
+  `send_goods_time` timestamp NULL DEFAULT NULL,
+  `send_goods_id` bigint(20) DEFAULT NULL,
+  `send_goods_state` int(2) DEFAULT '0',
   `confirm` int(1) DEFAULT '0',
-  `confirm_time` datetime DEFAULT NULL,
+  `confirm_time` timestamp NULL DEFAULT NULL,
   `state` int(2) DEFAULT '0',
-  PRIMARY KEY (`order_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`order_id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=150 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of tb_order
+-- ----------------------------
+BEGIN;
+INSERT INTO `tb_order` VALUES (132, 1, 1, 2, 1, 4.000, 1, '2018-05-01 13:17:44', 1, '2018-05-01 13:20:11', 21312, NULL, 1, '2018-05-01 13:20:16', 0);
+INSERT INTO `tb_order` VALUES (133, 1, 1, 2, 1, 4.000, 1, '2018-05-01 13:20:47', 1, '2018-05-01 13:21:37', 423423, NULL, 1, '2018-05-01 13:21:49', 0);
+INSERT INTO `tb_order` VALUES (134, 1, 1, 2, 1, 4.000, 1, '2018-05-01 13:22:35', 0, NULL, NULL, NULL, 0, NULL, 0);
+INSERT INTO `tb_order` VALUES (135, 1, 1, 2, 1, 4.000, 1, '2018-05-01 13:24:22', 0, NULL, NULL, NULL, 0, NULL, 0);
+INSERT INTO `tb_order` VALUES (137, 1, 1, 2, 1, 4.000, 1, '2018-05-01 13:24:37', 0, NULL, NULL, NULL, 0, NULL, 0);
+INSERT INTO `tb_order` VALUES (139, 1, 1, 2, 1, 4.000, 1, '2018-05-01 13:25:58', 0, NULL, NULL, NULL, 0, NULL, 0);
+INSERT INTO `tb_order` VALUES (141, 1, 1, 2, 1, 4.000, 1, '2018-05-01 13:35:31', 0, NULL, NULL, NULL, 0, NULL, 0);
+INSERT INTO `tb_order` VALUES (142, 1, 1, 2, 1, 4.000, 1, '2018-05-01 13:40:28', 0, NULL, NULL, NULL, 0, NULL, 0);
+INSERT INTO `tb_order` VALUES (143, 1, 1, 2, 10, 4.000, 1, '2018-05-01 13:44:06', 0, NULL, NULL, NULL, 0, NULL, 0);
+INSERT INTO `tb_order` VALUES (149, 1, 2, 7, 1, 1799.100, 1, '2018-05-01 14:15:14', 0, NULL, NULL, NULL, 0, NULL, 0);
+COMMIT;
 
 -- ----------------------------
 -- Table structure for tb_purchased
@@ -234,7 +245,15 @@ CREATE TABLE `tb_purchased` (
   `evaluation_time` datetime DEFAULT NULL,
   `state` int(2) DEFAULT '0',
   PRIMARY KEY (`purchased_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of tb_purchased
+-- ----------------------------
+BEGIN;
+INSERT INTO `tb_purchased` VALUES (12, 132, '2018-05-01 13:20:16', 1, '还可以的', '2018-05-01 13:20:26', 0);
+INSERT INTO `tb_purchased` VALUES (13, 133, '2018-05-01 13:21:49', 0, NULL, NULL, 0);
+COMMIT;
 
 -- ----------------------------
 -- Table structure for tb_receiving_address
@@ -299,8 +318,8 @@ CREATE TABLE `tb_shop` (
 -- Records of tb_shop
 -- ----------------------------
 BEGIN;
-INSERT INTO `tb_shop` VALUES (1, 1, 1, '两个人的小店', '卖一些零食', NULL, 90000.000, 01);
-INSERT INTO `tb_shop` VALUES (2, 2, 2, '九州小店', '服装', NULL, 190000.000, 01);
+INSERT INTO `tb_shop` VALUES (1, 1, 1, '两个人的小店', '卖一些零食', NULL, 90287.240, 01);
+INSERT INTO `tb_shop` VALUES (2, 2, 2, '九州小店', '服装', NULL, 195397.300, 01);
 INSERT INTO `tb_shop` VALUES (3, 1, NULL, '小小卖部', '卖零食的', NULL, NULL, 01);
 COMMIT;
 
@@ -310,7 +329,7 @@ COMMIT;
 DROP TABLE IF EXISTS `tb_user`;
 CREATE TABLE `tb_user` (
   `user_id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `username` char(16) NOT NULL,
+  `username` char(16) DEFAULT NULL,
   `password` char(16) DEFAULT NULL,
   `avatar_url` char(254) DEFAULT NULL,
   `nickname` char(20) DEFAULT NULL,
@@ -323,25 +342,24 @@ CREATE TABLE `tb_user` (
   `liveAddress` char(100) DEFAULT NULL,
   `money` double(11,3) DEFAULT '0.000',
   `payment_password` char(6) DEFAULT NULL,
-  PRIMARY KEY (`user_id`,`username`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`user_id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of tb_user
 -- ----------------------------
 BEGIN;
-INSERT INTO `tb_user` VALUES (1, 'user', 'user', NULL, '两个人', '陆鸿宇', 1, '17263527163', '1983-04-22', '73625@qq.com', '四川成都', '广东深圳', 4000.000, '888888');
-INSERT INTO `tb_user` VALUES (2, 'user1', 'user1', NULL, '用户1', '用户1', 0, NULL, NULL, NULL, NULL, NULL, 0.000, NULL);
-INSERT INTO `tb_user` VALUES (3, 'user2', 'user2', NULL, '用户2', '用户2', 0, NULL, NULL, NULL, NULL, NULL, 0.000, NULL);
-INSERT INTO `tb_user` VALUES (4, 'user3', 'user3', NULL, '用户3', '用户3', 1, NULL, NULL, NULL, NULL, NULL, 0.000, NULL);
-INSERT INTO `tb_user` VALUES (5, 'user4', 'user4', NULL, '用户4', '用户4', 1, NULL, NULL, NULL, NULL, NULL, 0.000, NULL);
-INSERT INTO `tb_user` VALUES (6, 'user5', 'user5', NULL, '用户5', '用户5', 0, NULL, NULL, NULL, NULL, NULL, 0.000, NULL);
-INSERT INTO `tb_user` VALUES (7, 'user6', 'user6', NULL, '用户6', '用户6', 1, NULL, NULL, NULL, NULL, NULL, 0.000, NULL);
-INSERT INTO `tb_user` VALUES (8, 'user7', 'user7', NULL, '用户7', '用户7', 0, NULL, NULL, NULL, NULL, NULL, 0.000, NULL);
-INSERT INTO `tb_user` VALUES (9, 'user8', 'user8', NULL, '用户8', '用户8', 0, NULL, NULL, NULL, NULL, NULL, 0.000, NULL);
-INSERT INTO `tb_user` VALUES (10, 'user9', 'user9', NULL, '用户9', '用户9', 0, NULL, NULL, NULL, NULL, NULL, 0.000, NULL);
-INSERT INTO `tb_user` VALUES (11, 'user10', 'user10', NULL, '用户10', '用户10', 1, NULL, NULL, NULL, NULL, NULL, 0.000, NULL);
-INSERT INTO `tb_user` VALUES (14, 'user11', 'user11', NULL, NULL, NULL, 1, NULL, NULL, NULL, NULL, NULL, 0.000, NULL);
+INSERT INTO `tb_user` VALUES (1, 'user', 'user', NULL, '两个人', '陆鸿宇', 1, '17263527163', '1983-04-22', '73625@qq.com', '四川成都', '广东深圳', 178801.460, '123123');
+INSERT INTO `tb_user` VALUES (2, 'user1', 'user1', NULL, '用户1', '用户1', 0, NULL, NULL, NULL, NULL, NULL, 3192.000, '123123');
+INSERT INTO `tb_user` VALUES (3, 'user2', 'user2', NULL, '用户2', '用户2', 0, NULL, NULL, NULL, NULL, NULL, 0.000, '');
+INSERT INTO `tb_user` VALUES (4, 'user3', 'user3', NULL, '用户3', '用户3', 1, NULL, NULL, NULL, NULL, NULL, 0.000, '');
+INSERT INTO `tb_user` VALUES (5, 'user4', 'user4', NULL, '用户4', '用户4', 1, NULL, NULL, NULL, NULL, NULL, 0.000, '');
+INSERT INTO `tb_user` VALUES (6, 'user5', 'user5', NULL, '用户5', '用户5', 0, NULL, NULL, NULL, NULL, NULL, 0.000, '');
+INSERT INTO `tb_user` VALUES (7, 'user6', 'user6', NULL, '用户6', '用户6', 1, NULL, NULL, NULL, NULL, NULL, 0.000, '');
+INSERT INTO `tb_user` VALUES (8, 'user7', 'user7', NULL, '用户7', '用户7', 0, NULL, NULL, NULL, NULL, NULL, 0.000, '');
+INSERT INTO `tb_user` VALUES (9, 'user8', 'user8', NULL, '用户8', '用户8', 0, NULL, NULL, NULL, NULL, NULL, 0.000, '');
+INSERT INTO `tb_user` VALUES (10, 'user9', 'user9', NULL, '用户9', '用户9', 0, NULL, NULL, NULL, NULL, NULL, 0.000, '');
+INSERT INTO `tb_user` VALUES (11, 'user10', 'user10', NULL, '用户10', '用户10', 1, NULL, NULL, NULL, NULL, NULL, 0.000, '');
 COMMIT;
 
 -- ----------------------------
@@ -354,17 +372,19 @@ CREATE TABLE `tb_warehouse` (
   `goods_id` bigint(20) DEFAULT NULL,
   `number` int(11) DEFAULT NULL,
   PRIMARY KEY (`warehouse_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of tb_warehouse
 -- ----------------------------
 BEGIN;
-INSERT INTO `tb_warehouse` VALUES (1, 1, 1, 60);
-INSERT INTO `tb_warehouse` VALUES (2, 1, 2, 54);
-INSERT INTO `tb_warehouse` VALUES (3, 1, 3, 45);
-INSERT INTO `tb_warehouse` VALUES (4, 1, 4, 43);
-INSERT INTO `tb_warehouse` VALUES (5, 1, 5, 98);
+INSERT INTO `tb_warehouse` VALUES (1, 1, 1, 48);
+INSERT INTO `tb_warehouse` VALUES (2, 1, 2, 27);
+INSERT INTO `tb_warehouse` VALUES (3, 1, 3, 37);
+INSERT INTO `tb_warehouse` VALUES (4, 1, 4, 41);
+INSERT INTO `tb_warehouse` VALUES (5, 1, 5, 96);
+INSERT INTO `tb_warehouse` VALUES (6, 2, 6, 100);
+INSERT INTO `tb_warehouse` VALUES (7, 2, 7, 97);
 COMMIT;
 
 -- ----------------------------
